a95b3454e2bcf22e1a1f058628b04b5762163e56
joshuatxtcllc <jaysframes@gmail.com>
2025-03-06 12:25:31 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Frame Guru</h1>
-        <p>Custom Framing Redefined</p>
-      </header>
+    <div>
+      <h1>The Frame Guru</h1>
+      <p>Custom Framing Redefined</p>
     </div>
   );
 }
