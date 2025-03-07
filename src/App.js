67cfc2626ac418200851774c91ebd5cec9d4cde0
joshuatxtcllc <jaysframes@gmail.com>
2025-03-06 18:20:09 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,3 +10,31 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+function App({ title, subtitle }) {
+  return (
+    <Container>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </Container>
+  );
+}
+
+App.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+};
+
+App.defaultProps = {
+  title: 'The Frame Guru',
+  subtitle: 'Custom Framing Redefined',
+};
+
+export default App;
